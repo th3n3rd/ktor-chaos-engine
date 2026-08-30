11291e1b1edfe931fba34ed6dc57024e62fdfabe
Marco Garofalo <marcogarofalo.personal@gmail.com>
2026-08-30 22:01:44 +0100

    allow to trigger a behaviour based on a percentage

--- src/main/kotlin/io/th3n3rd/ktor/client/chaos/ChaosTriggers.kt
+++ src/main/kotlin/io/th3n3rd/ktor/client/chaos/ChaosTriggers.kt
@@ -1,6 +1,9 @@
 package io.th3n3rd.ktor.client.chaos
 
 import io.ktor.client.request.*
+import io.th3n3rd.ktor.client.chaos.ChaosTriggers.Percentage
+import io.th3n3rd.ktor.client.chaos.ChaosTriggers.Times
+import java.util.*
 import java.util.concurrent.atomic.AtomicInteger
 
 fun interface ChaosTrigger {
@@ -17,6 +20,14 @@ object ChaosTriggers {
             return ChaosTrigger { remaining.getAndDecrement() > 0 }
         }
     }
+
+    object Percentage {
+        operator fun invoke(value: Int, random: Random = Random()) = ChaosTrigger { request ->
+            require(value in 0..100) { "$value should be between 0 and 100" }
+            random.nextInt(100) < value
+        }
+    }
 }
 
-fun Int.times() = ChaosTriggers.Times(this@times)
+fun Int.times() = Times(this@times)
+fun Int.percent(random: Random = Random()) = Percentage(this@percent, random)
--- src/test/kotlin/io/th3n3rd/ktor/client/chaos/ChaosTriggerTests.kt
+++ src/test/kotlin/io/th3n3rd/ktor/client/chaos/ChaosTriggerTests.kt
@@ -1,9 +1,11 @@
 package io.th3n3rd.ktor.client.chaos
 
+import io.kotest.assertions.withClue
 import io.kotest.matchers.shouldBe
 import io.th3n3rd.ktor.client.chaos.ChaosTriggers.Always
 import io.th3n3rd.ktor.client.chaos.ChaosTriggers.Never
 import org.junit.jupiter.api.Test
+import java.util.*
 
 class ChaosTriggerTests {
 
@@ -21,15 +23,36 @@ class ChaosTriggerTests {
 
     @Test
     fun `triggers exactly n times`() {
-        with (1.times()) {
+        with(1.times()) {
             this(anyRequest().build()) shouldBe true
             this(anyRequest().build()) shouldBe false
         }
 
-        with (2.times()) {
+        with(2.times()) {
             this(anyRequest().build()) shouldBe true
             this(anyRequest().build()) shouldBe true
             this(anyRequest().build()) shouldBe false
+        }
+    }
+
+    @Test
+    fun `triggers n percent of the time`() {
+        val random = Random(0)
+
+        with(0.percent(random)) {
+            withClue("0 percent never triggers") {
+                repeat(10_000) {
+                    this(anyRequest().build()) shouldBe false
+                }
+            }
+        }
+
+        with(100.percent(random)) {
+            withClue("100 percent always triggers") {
+                repeat(10_000) {
+                    this(anyRequest().build()) shouldBe true
+                }
+            }
         }
     }
 }
