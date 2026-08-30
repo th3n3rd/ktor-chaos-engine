db85a4a53b17d2e144157b29255f9eb60ac22f78
Marco Garofalo <marcogarofalo.personal@gmail.com>
2026-08-30 18:38:57 +0100

    allow to trigger a behaviour exactly n times

--- src/main/kotlin/io/th3n3rd/ktor/client/chaos/ChaosTriggers.kt
+++ src/main/kotlin/io/th3n3rd/ktor/client/chaos/ChaosTriggers.kt
@@ -1,6 +1,7 @@
 package io.th3n3rd.ktor.client.chaos
 
 import io.ktor.client.request.*
+import java.util.concurrent.atomic.AtomicInteger
 
 fun interface ChaosTrigger {
     operator fun invoke(request: HttpRequestData): Boolean
@@ -9,4 +10,13 @@ fun interface ChaosTrigger {
 object ChaosTriggers {
     val Always = ChaosTrigger { true }
     val Never = ChaosTrigger { false }
+
+    object Times {
+        operator fun invoke(exactly: Int): ChaosTrigger {
+            val remaining = AtomicInteger(exactly)
+            return ChaosTrigger { remaining.getAndDecrement() > 0 }
+        }
+    }
 }
+
+fun Int.times() = ChaosTriggers.Times(this@times)
--- src/test/kotlin/io/th3n3rd/ktor/client/chaos/ChaosTriggerTests.kt
+++ src/test/kotlin/io/th3n3rd/ktor/client/chaos/ChaosTriggerTests.kt
@@ -18,4 +18,18 @@ class ChaosTriggerTests {
         Never(anyRequest().build()) shouldBe false
         Never(anyRequest().build()) shouldBe false
     }
+
+    @Test
+    fun `triggers exactly n times`() {
+        with (1.times()) {
+            this(anyRequest().build()) shouldBe true
+            this(anyRequest().build()) shouldBe false
+        }
+
+        with (2.times()) {
+            this(anyRequest().build()) shouldBe true
+            this(anyRequest().build()) shouldBe true
+            this(anyRequest().build()) shouldBe false
+        }
+    }
 }
